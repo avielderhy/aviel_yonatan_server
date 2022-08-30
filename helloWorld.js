f98fa0a263293e1fca844a903ffb776eb261f210
Aviel Derhy <avielderhy@gmail.com>
2022-08-30 17:50:55 +0300

    Finished with the app

--- helloWorld.js
+++
@@ -1,5 +0,0 @@
-
-
-exports.helloWroldFunc = () => {
-    return "hello world"
-}
--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const app = express()
 const isPalindrome = require('./isPalindrome.js')
 const port = 5000
 
-app.get('/Palindrome/:string', (req, res) => {
-  res.status(200).send(isPalindrome.checkPalindrome(req.params.string))
-})
 
 app.listen(port, () => {
   console.log(`Our app listening on port ${port}`)
@@ -15,10 +12,10 @@ app.get('/helloWorld', function (req, res) {
   res.send('Hello World');
 })
 
+app.get('/isPalindrome/:string', (req, res) => {
+  res.status(200).send(isPalindrome.checkPalindrome((req.params.string)))
+})
 
 exports.stop = function () {
   server.close();
 }
-const server = app.listen(port, function () {
-  console.log("Express server listening on port %d in %s mode", port, app.settings.env);
-});
