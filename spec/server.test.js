1b7c48e867a1dd056a814d904777d729c0a5f672
Aviel Derhy <avielderhy@gmail.com>
2022-08-30 17:19:04 +0300

    tests modified

--- spec/server.test.js
+++ spec/server.test.js
@@ -4,14 +4,14 @@ const base_url = "http://localhost:3000/";
 
 describe("when testing projects endpoint", () => {
     test("should return status OK", (done) => {
-        axios.get(base_url+"isPrime/11").then(response => {
+        axios.get(base_url+"helloWorld").then(response => {
             expect(response.status).toBe(200);
             done();
            })
     });
     
     test("should return list of projects",(done) => {
-        axios.get(base_url+'isPrime/11').then(response => {
+        axios.get(base_url+'helloWorld').then(response => {
             expect(response.data).toBe(true);
             done();
         })
